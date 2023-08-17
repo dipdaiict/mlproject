0c6e9b851124c63ed1224d571d7cbd071de5fde4
Dip <dippatel256@gmail.com>
2023-08-17 23:06:36 +0530

    Added & Committed Logging & Exception

---
+++ src/exception.py
@@ -0,0 +1,30 @@
+import sys
+import logging
+
+def error_message_detail(error, error_detail:sys):
+    # error_detail.exc_info()
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error Occured In Python Script Name [{0}] Line Number [{1}] Error Message".format(
+        file_name,exc_tb.tb_lineno,str(error))
+    return error_message    
+    
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+
+    def __str__(self) -> str:
+        return self.error_message 
+
+
+if __name__=="__main__":
+
+    try:
+        a = 1/0
+
+    except Exception as e:
+        logging.info("Divide by Zero")    
+        raise CustomException (e, sys)
+
